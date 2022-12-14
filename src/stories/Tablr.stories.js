906e3dfd20bfa12dc76e989cc0c1c9bc66b8d98b
Hexia Valten <anaisrevellat@gmail.com>
2022-12-14 14:50:51 +0100

    test

--- src/stories/Tablr.stories.js
+++ src/stories/Tablr.stories.js
@@ -3,6 +3,9 @@ import { Tablr } from '../Tablr';
 export default {
     title: 'Tablr - A Customizable React Table Component',
     component: Tablr,
+    argTypes: {
+      rows: { control: '' }
+    }
 };
 
 const Template = args => <Tablr {...args} />;
@@ -10,11 +13,11 @@ const Template = args => <Tablr {...args} />;
 export const Default = Template.bind({});
 
 Default.args = {
+  headers: ['Col 1', 'Col 2', 'Col 3', 'Col 4', 'Col 5'],
   rows: [
     ['This', 'is', 'just', 'a', 'test'],
     ['This', 'is', 'also', 'a', 'test'],
     ['Just', 'a', 'little', 'more', 'data'],
     ['Row', 'number', 'four', 'right', 'here'],
   ],
-  headers: ['Col 1', 'Col 2', 'Col 3', 'Col 4', 'Col 5'],
 };
